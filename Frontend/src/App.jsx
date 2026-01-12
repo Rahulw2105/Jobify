b40a48cfb95e899b4d8eda83cc161e9467b9ee72
Rahul Wankhede <wankhederahul116@gmail.com>
2026-01-12 12:56:21 +0530

    React router setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './App.css'
 
 function App() {
 
-
+const appRoutes = createBrowserRouter([
+  {
+    path:"/",
+    element:<h1>Home Page...</h1>
+  },
+   {
+    path:"/about",
+    element:<h1>About page...</h1>
+  }
+])
   return (
     <>
-        <h1>Jobify</h1>
+       <RouterProvider router={appRoutes}/>
     </>
   )
 }

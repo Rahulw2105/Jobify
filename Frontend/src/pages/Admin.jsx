3f54665b8698ab2cfb1172c5eaa3ee511d17593e
Rahul Wankhede <wankhederahul116@gmail.com>
2026-01-13 17:35:39 +0530

    Add frontend page componenets

---
+++ Frontend/src/pages/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+      Admin PAge
+    </div>
+  )
+}
+
+export default Admin
---
+++ Frontend/src/pages/EditJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditJob = () => {
+  return (
+    <div>
+      Edit Jobs page...
+    </div>
+  )
+}
+
+export default EditJob
